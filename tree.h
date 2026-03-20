8ea28e22a45db83a88aec99ce9e5dd182172298d
aaaba12321 <aaaba12321@gmail.com>
2026-03-20 14:58:13 +0900

    Update tree.h
    size() 추가
    clear() 추가
    bfs() 개선

--- tree.h
+++ tree.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <queue>
+#include <vector>
 struct node {
 	int v;
 	node* l;
@@ -14,9 +15,11 @@ struct node {
 class tree {
 private:
 	node* root;
+	int sot;
 	void insert_r(int x, node* cur) {
 		if (x < cur->v) {
 			if (!cur->l) {
+				sot++;
 				cur->l = new node();
 				cur->l->v = x;
 				return;
@@ -26,6 +29,7 @@ private:
 		}
 		if (x > cur->v) {
 			if (!cur->r) {
+				sot++;
 				cur->r = new node();
 				cur->r->v = x;
 				return;
@@ -54,6 +58,7 @@ private:
 			cur->r = remove_r(cur->r, r);
 		}
 		else {
+			sot--;
 			if (!cur->l) {
 				node* temp = cur->r;
 				delete cur;
@@ -70,6 +75,16 @@ private:
 		}
 		return cur;
 	}
+	void clear_r(node* node) {
+		if (node->l) {
+			clear_r(node->l);
+		}
+		if (node->r) {
+			clear_r(node->r);
+		}
+		delete node;
+		sot--;
+	}
 public:
 	tree() {
 		root = nullptr;
@@ -77,34 +92,67 @@ public:
 	void insert(int x) {
 		if (!root) {
 			root = new node();
+			sot++;
 			root->v = x;
 			return;
 		}
 		insert_r(x, root);
 	}
-	void bfs() {
+	void bfs() { //show empty
 		if (!root) {
 			return;
 		}
 		std::queue<node*> q;
+		std::queue<int> work;//-1:do from q, else:output empty space(amount:the value)
 		q.push(root);
+		work.push(-1);
 		while (!q.empty()) {
-			int size = q.size();
-			for (int i = 0; i < size; i++) {
+			int s = work.size();
+			for (int i = 0; i < s; i++) {
+				int v = work.front();
+				work.pop();
+				if (v != -1) {
+					for (int j = 0; j < v; j++) {
+						std::cout << "- ";
+					}
+					work.push(v * 2);
+					continue;
+				}
 				node* t = q.front();
 				q.pop();
 				std::cout << t->v << ' ';
 				if (t->l) {
 					q.push(t->l);
+					work.push(-1);
+				}
+				else {
+					work.push(1);
 				}
 				if (t->r) {
 					q.push(t->r);
+					work.push(-1);
+				}
+				else {
+					work.push(1);
 				}
 			}
 			std::cout << '\n';
 		}
 	}
 	void remove(int x) {
+		if (!root) {
+			return;
+		}
 		remove_r(root, x);
+	}
+	void clear() {
+		if (!root) {
+			return;
+		}
+		clear_r(root);
+		root = nullptr;
+	}
+	int size() {
+		return sot;
 	}
 };
